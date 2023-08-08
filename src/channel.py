71fb0126e2a88e8f8e5ee983adae59cbe97c9f99
an1meh <adolf.ddd@mail.ru>
2023-08-08 19:03:45 +0300

    add basic functionality

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+import os
+import json
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    API_KEY = os.getenv("API_KEY")
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
         pass
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
