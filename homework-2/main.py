b4387024bec78fa68dfdd2f8e9d310e6f15cb9a6
an1meh <adolf.ddd@mail.ru>
2023-08-16 21:43:50 +0300

    homework 2

--- homework-2/main.py
+++ homework-2/main.py
@@ -2,14 +2,14 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-
     # получаем значения атрибутов
     print(moscowpython.title)  # MoscowPython
     print(moscowpython.video_count)  # 685 (может уже больше)
     print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
-    moscowpython.channel_id = 'Новое название'
+   # moscowpython.channel_id = 'Новое название'
+    #print(moscowpython.channel_id)
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
--- src/channel.py
+++ src/channel.py
@@ -10,11 +10,64 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        pass
+        # id канала
+        self.__channel_id = channel_id
+        # название канала
+        self.title = \
+            self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()["items"][0][
+                "snippet"]["title"]
+        # описание канала
+        self.channel_description = \
+            self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()["items"][0][
+                "snippet"][
+                "description"]
+        # ссылка на канал
+        self.url = \
+            self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()["items"][0][
+                "snippet"][
+                "thumbnails"]["default"]["url"]
+        # количество подписчиков
+        self.subscriber_count = \
+            self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()["items"][0][
+                "statistics"][
+                "subscriberCount"]
+        # количество видео
+        self.video_count = \
+            self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()["items"][0][
+                "statistics"][
+                "videoCount"]
+        # количество просмотров
+        self.view_count = \
+            self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()["items"][0][
+                "statistics"][
+                "viewCount"]
+
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, filename):
+        data = {
+            "channel_id": self.__channel_id,
+            "title": self.title,
+            "channel_description": self.channel_description,
+            "url": self.url,
+            "subscriber_count": self.subscriber_count,
+            "video_count": self.video_count,
+            "view_count": self.view_count
+        }
+
+        with open(filename, "w") as f:
+            f.write(json.dumps(data))
